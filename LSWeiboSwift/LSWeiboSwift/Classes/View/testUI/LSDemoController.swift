698951cde09963486aefc39d6ba2b3a84c04307f
LeeMinglu <402007353@qq.com>
2017-05-16 20:21:13 +0800

    push多个控制器

--- LSWeiboSwift/LSWeiboSwift/Classes/View/testUI/LSDemoController.swift
+++ LSWeiboSwift/LSWeiboSwift/Classes/View/testUI/LSDemoController.swift
@@ -13,14 +13,23 @@ class LSDemoController: LSBaseController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        title = "第 +  \(navigationController?.childViewControllers.count ?? 0) 个"
         
         print("进入DeMo控制器")
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @objc fileprivate func nextVC() {
+        navigationController?.pushViewController(LSDemoController(), animated: true)
     }
 }
 
 extension LSDemoController {
     override func setupUI() {
         super.setupUI()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "下一个", style: .plain, target: self, action: #selector(nextVC))
     }
 }
