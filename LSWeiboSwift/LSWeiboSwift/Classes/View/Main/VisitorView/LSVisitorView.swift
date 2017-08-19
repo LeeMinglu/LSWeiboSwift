ce1507faaa7f4d9dc11e491a9fcff6a1ceb53038
LeeMinglu <402007353@qq.com>
2017-08-19 10:41:58 +0800

    VisitorView加入扩展

--- LSWeiboSwift/LSWeiboSwift/Classes/View/Main/VisitorView/LSVisitorView.swift
+++ LSWeiboSwift/LSWeiboSwift/Classes/View/Main/VisitorView/LSVisitorView.swift
@@ -12,12 +12,17 @@ class LSVisitorView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.backgroundColor = UIColor.white
+        setupUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+}
+
+extension LSVisitorView {
+    func setupUI() {
+        self.backgroundColor = UIColor.white
+    }
 }
