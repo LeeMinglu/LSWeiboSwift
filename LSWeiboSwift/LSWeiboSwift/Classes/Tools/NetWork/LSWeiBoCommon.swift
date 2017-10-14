5ac860044b5a4231b59423d2bb31bf94eb2fa517
LeeMinglu <402007353@qq.com>
2017-10-14 22:01:30 +0800

    添加应用程序信息定义

--- LSWeiboSwift/LSWeiboSwift/Classes/Tools/NetWork/LSWeiBoCommon.swift
+++ LSWeiboSwift/LSWeiboSwift/Classes/Tools/NetWork/LSWeiBoCommon.swift
@@ -8,4 +8,15 @@
 
 import Foundation
 
+// MARK: -----应用程序信息------
+//应用程序ID
+let AppID = "3123639654"
+//应用程序加密信息
+let AppSecret = "7a1010a34ac579cf8cc5c9a1b636cbfa"
+//回调URI--登录程序跳转的URI,参数以get形式进行拼接
+let RedictURI = "https://www.baidu.com"
+
+
+// MARK: -----全局通知定义-----
+//登陆通知
 let LSUserShouldeLoginNotification = "LSUserShouldeLoginNotification"
