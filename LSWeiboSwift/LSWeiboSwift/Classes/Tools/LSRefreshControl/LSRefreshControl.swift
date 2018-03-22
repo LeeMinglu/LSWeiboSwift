7f5750646a237b3f69fe8f04c50343b48e3b7c54
LeeMinglu <402007353@qq.com>
2018-03-22 21:35:18 +0800

    添加自定义refresh控件，替换系统的refresh控件，争取最小的改动

---
+++ LSWeiboSwift/LSWeiboSwift/Classes/Tools/LSRefreshControl/LSRefreshControl.swift
@@ -0,0 +1,24 @@
+//
+//  LSRefreshControl.swift
+//  LSWeiboSwift
+//
+//  Created by 李明禄 on 2018/3/22.
+//  Copyright © 2018年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class LSRefreshControl: UIControl {
+
+    //开始刷新
+    func beginRefreshing() {
+    
+    }
+    
+    //结束刷新
+    func endRefreshing() {
+    
+    }
+
+
+}
