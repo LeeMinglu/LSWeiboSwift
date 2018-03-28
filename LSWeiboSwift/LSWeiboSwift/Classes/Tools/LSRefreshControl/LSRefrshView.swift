6ca3a49caa3d2137ac8f4439ae9dbcb9284cb778
LeeMinglu <402007353@qq.com>
2018-03-28 23:26:40 +0800

    新建刷新视图连线并布局

---
+++ LSWeiboSwift/LSWeiboSwift/Classes/Tools/LSRefreshControl/LSRefrshView.swift
@@ -0,0 +1,21 @@
+//
+//  LSRefrshView.swift
+//  刷新控件002
+//
+//  Created by 李明禄 on 2018/3/28.
+//  Copyright © 2018年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class LSRefrshView: UIView {
+
+    //提示标签
+    @IBOutlet weak var RefreshLabel: UILabel!
+    //提示图标
+    @IBOutlet weak var iconView: UIImageView!
+    //指示器
+    @IBOutlet weak var Indicator: UIActivityIndicatorView!
+    
+
+}
