f4b13b3d23828770a26904cb76f57450b7431706
LeeMinglu <402007353@qq.com>
2018-04-20 21:33:26 +0800

    添加目录及加载MainViewController

--- LSWeiboSwift/LSWeiboSwift/AppDelegate.swift
+++ LSWeiboSwift/LSWeiboSwift/AppDelegate.swift
@@ -15,7 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let rootViewController = LSMainViewController()
+        window = UIWindow()
+        
+        window?.rootViewController = rootViewController
+        
+        window?.makeKeyAndVisible()
+        
+        
+        
+        
+        
         return true
     }
 
